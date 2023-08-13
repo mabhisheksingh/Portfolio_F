145bfe832e26aee80b501cfe447da2af31dc5c83
Abhishek Singh <87180842+mabhisheksingh@users.noreply.github.com>
2023-08-13 10:40:28 +0530

    Add federation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    federation({
+      name: 'Games',
+      remotes: {
+        remoteApp: 'https://master--peppy-naiad-3f5858.netlify.app/assets/Game.js',
+      },
+      shared: ['react','react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
