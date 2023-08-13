6e5d33ff0d98ff959ff1eb3b57d3f0bda30c4da0
Abhishek Singh <87180842+mabhisheksingh@users.noreply.github.com>
2023-08-13 12:46:46 +0530

    chnages done for repo

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import federation from '@originjs/vite-plugin-federation'
 
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react(),
     federation({
-      name: 'Games',
+      name: 'Remote_Repo',
       remotes: {
-        remoteApp: 'https://master--peppy-naiad-3f5858.netlify.app/assets/Game.js',
+        Game: env.VITE_MODULE_FEDERATION_GAME,
       },
       shared: ['react','react-dom']
     })
@@ -19,4 +25,5 @@ export default defineConfig({
     minify: false,
     cssCodeSplit: false
   }
+  
 })
